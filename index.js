56db91deb94d63ee860776d546bbbbd1175d0215
smlsunxie <smlsun@gmail.com>
2018-04-25 12:14:42 +0800

    修正非同步執行問題

--- index.js
+++ index.js
@@ -40,28 +40,28 @@ class MQHelper {
         let connection = await this.getConnection();
         this.connection = connection;
         let channel = await connection.createChannel();
-        this.channel = channel;        
+        this.channel = channel;
     };
 
 
 
 
     async queueSend(param) {
-        
-        let {handlerName, data, name} = param;
-        if(name == null) name = "default";
+
+        let { handlerName, data, name } = param;
+        if (name == null) name = "default";
 
         let sendName = name;
 
-        try {    
+        try {
             // 建立一個自己的 queue
             console.log("name", sendName);
             let q = await this.channel.assertQueue(sendName);
-    
+
             let content = {
                 data: data
             };
-    
+
             // 送出 rpc 請求
 
             return await this.channel.sendToQueue(
@@ -69,24 +69,30 @@ class MQHelper {
                 new Buffer(JSON.stringify(content)),
                 { replyTo: q.queue }
             );
-    
+
         } catch (err) {
             console.error(err);
         }
     };
     async queueConsume(params) {
-        try {    
-            let {name, handler} = params;
-            if(name == null) name = "default";
-            let consumeName = name ;
+        try {
+            let { name, handler } = params;
+            if (name == null) name = "default";
+            let consumeName = name;
             console.log("consumeName", consumeName)
-            let q = await this.channel.assertQueue(consumeName);    
-            this.channel.consume(q.queue, handler(this));
-
+            let q = await this.channel.assertQueue(consumeName, { durable: true });
+            this.channel.prefetch(1)
+            this.channel.consume(q.queue, handler(this), { noAck: false });
+            // 參考 https://zhuanlan.zhihu.com/p/28276010
+            // 透過 下面控制依序執行
+            // execute(params).then(() => {
+            //     mqHelper.channel.ack(msg);
+            // });
+            
         } catch (err) {
             console.error(err);
         }
-    };  
+    };
 
     async hello(props) {
         return await new Promise(resolve => {
